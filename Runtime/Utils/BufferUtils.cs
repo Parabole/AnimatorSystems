01de653f57acf0a56dc9af7edc452eb585cc5458
Alexandre Fiset <alexandre.fiset@gmail.com>
2019-09-12 13:28:49 -0400

    Added utils to simplify adding elements to buffers

---
+++ Runtime/Utils/BufferUtils.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Parabole.AnimatorSystems
+{
+    public static class BufferUtils 
+    {
+        public static void AddFloatToBuffer(int hash, float value, DynamicBuffer<FloatParameter> buffer)
+        {
+            var element = new FloatParameter()
+            {
+                Parameter = hash,
+                Value = value
+            };
+            buffer.Add(element);
+        }
+        
+        public static void AddBoolToBuffer(int hash, bool value, DynamicBuffer<BoolParameter> buffer)
+        {
+            var element = new BoolParameter()
+            {
+                Parameter = hash,
+                Value = value
+            };
+            buffer.Add(element);
+        }
+        
+        public static void AddIntToBuffer (int hash, int value, DynamicBuffer<IntParameter> buffer)
+        {
+            var element = new IntParameter()
+            {
+                Parameter = hash,
+                Value = value
+            };
+            buffer.Add(element);
+        }
+        
+        public static void AddTriggerToBuffer (int hash, int value, DynamicBuffer<IntParameter> buffer)
+        {
+            var element = new IntParameter()
+            {
+                Parameter = hash,
+                Value = value
+            };
+            buffer.Add(element);
+        }
+    }
+
+}
+
